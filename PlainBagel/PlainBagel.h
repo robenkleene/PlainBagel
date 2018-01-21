900ca0e7c2837b8faee94d6b3716c4fab771e660
Roben Kleene <robenk@gmail.com>
2018-01-20 19:12:53 -0500

    First commit

---
+++ PlainBagel/PlainBagel.h
@@ -0,0 +1,19 @@
+//
+//  PlainBagel.h
+//  PlainBagel
+//
+//  Created by Roben Kleene on 1/20/18.
+//  Copyright © 2018 Roben Kleene. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for PlainBagel.
+FOUNDATION_EXPORT double PlainBagelVersionNumber;
+
+//! Project version string for PlainBagel.
+FOUNDATION_EXPORT const unsigned char PlainBagelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlainBagel/PublicHeader.h>
+
+
